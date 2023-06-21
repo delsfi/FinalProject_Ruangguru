1ff53f8dfdc74345b81c992241913c77c644df55
delsfi <aloisiusfidel1@gmail.com>
2023-06-21 23:10:19 +0700

    ruangguru

---
+++ README.md
@@ -0,0 +1,14 @@
+# Web Application
+Tampilan index
+![Index Page](./assets/index1.png)
+Tampilan Login
+![Index Page](./assets/login1.png)
+Tampilan Register
+![Index Page](./assets/register1.png)
+Tampilan Dashboard
+![Index Page](./assets/dashboard.png)
+Tampilan Task
+![Index Page](./assets/task.png)
+Tampilan Category
+![Index Page](./assets/category.png)
+## Final Course Project - Task Tracker Plus
---
+++ middleware/auth.go
@@ -0,0 +1,41 @@
+package middleware
+
+import (
+	"a21hc3NpZ25tZW50/model"
+	"net/http"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+func Auth() gin.HandlerFunc {
+	return gin.HandlerFunc(func(ctx *gin.Context) {// TODO: answer here
+		data, err := ctx.Cookie("session_token")
+		if err != nil {
+			if err == http.ErrNoCookie {
+				if ctx.GetHeader("Content-Type") == "application/json" {
+					ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+				} else {
+					ctx.Redirect(http.StatusSeeOther, "/user/login")
+					ctx.Abort()
+				}
+				return
+			}
+			ctx.AbortWithStatus(http.StatusBadRequest)
+			return
+		}
+
+		claims := &model.Claims{}
+
+		tkn, err := jwt.ParseWithClaims(data, claims, func(token *jwt.Token) (interface{}, error) {
+            return model.JwtKey, nil
+        })
+        if err != nil || !tkn.Valid {
+            ctx.JSON(400, model.ErrorResponse{Error: "ga valid bang"})
+            return
+        }
+		ctx.Set("email", claims.Email)
+		ctx.Next()
+	})
+	// TODO: answer here
+}
